d84b95fc57c82ccc981682a1ca3543c6c97bf8c9
Dheeraj <Dheeraj77253@gmail.com>
2020-07-27 18:27:05 +0530

    add test for checking already registered patient

---
+++ test/patient.js
@@ -0,0 +1,41 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../index");
+
+//Assertion style
+const should = chai.should();
+chai.use(chaiHttp);
+
+const authorizationToken =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjFkNzEyYTI3ZTU3YTQyMmM4YjZhZGQiLCJwaG9uZSI6IjcwNjc3NzcyNTMiLCJuYW1lIjoiRGhlZXJhaiBLdXNod2FoIiwiaWF0IjoxNTk1ODQ0NTY1LCJleHAiOjE1OTU5NDQ1NjV9.csjLNgtxiumsixGUbgL1BSbVqlRPAmZ2yfthebe0L50";
+
+describe("Hospital-API", () => {
+  describe("/GET /patient/register", () => {
+    it("Check for Patient Already exist with these number", (done) => {
+      chai
+        .request("http://localhost:8000/")
+        .post("patients/register")
+        .set({
+          "content-type": "application/x-www-form-urlencoded",
+          Authorization: authorizationToken,
+        })
+        .send({
+          name: "VERMA",
+          phone: "1595846896370",
+        })
+        .end((err, response) => {
+          if (err) {
+            console.log(err);
+          }
+          response.should.have.status(200);
+          response.body.data.should.have.property("patient");
+          response.body.data.patient.should.have.property("_id");
+          response.body.should.have.property("message");
+          response.body.message.should.be.eql(
+            "Patient already exist with this mobile number"
+          );
+          done();
+        });
+    });
+  });
+});
