6410b96f9c089ec71da33b69d0b56405631a01d5
Dheeraj <Dheeraj77253@gmail.com>
2020-07-27 19:46:16 +0530

    add test case to get all report and passing invalid patient ID

--- test/all_report.js
+++ test/all_report.js
@@ -26,5 +26,22 @@ describe("Hospital-API", () => {
           done();
         });
     });
+    //check case when invalid patient ID is passed for getting report
+    it("Check for Invalid patient ID passed for getting all reports", (done) => {
+      chai
+        .request("http://localhost:8000/")
+        .get("patients/1232232/all_reports") //passing wrong patient ID
+        .end((err, response) => {
+          if (err) {
+            console.log(err);
+          }
+          //checking for various property to validate response object
+          response.should.have.status(402);
+          response.body.should.have.property("success").eql(false);
+          response.body.should.have.property("message");
+          response.body.message.should.be.eql("Invalid Patient ID");
+          done();
+        });
+    });
   });
 });
