6ddbe44c0abbfa6d306d6664cc9c711c908fdc93
Dheeraj <Dheeraj77253@gmail.com>
2020-07-27 20:14:48 +0530

    add test case to check report with report code

--- test/4_report_with_status.js
+++ test/4_report_with_status.js
@@ -20,6 +20,7 @@ describe("Hospital-API", () => {
           //checking for various property to validate response object
           response.should.have.status(200);
           response.body.should.be.a("object");
+          response.body.should.have.property("success").eql(true);
           response.body.data.should.have.property("reports");
           response.body.should.have.property("message");
           response.body.message.should.be.eql(
@@ -28,7 +29,7 @@ describe("Hospital-API", () => {
           done();
         });
     });
-    //Getting all the report with particular status
+    //checking getting all report by passing invalid status
     it("Check for getting all the reports by passing Invalid Status", (done) => {
       status = "Admitted";
       chai
@@ -40,11 +41,51 @@ describe("Hospital-API", () => {
           }
           //checking for various property to validate response object
           response.should.have.status(402);
+          response.body.should.have.property("success").eql(false);
           response.body.should.be.a("object");
           response.body.should.have.property("message");
           response.body.message.should.be.eql(
             "Status value is incorrect...values can be only Negative, Travelled-Quarantine, Symptoms-Quarantine, Positive-Admit"
           );
+          done();
+        });
+    });
+    //checking getting report by passing report code
+    it("Check for getting report by passing Report Code", (done) => {
+      let reportCode = "1595765326089";
+      chai
+        .request("http://localhost:8000/")
+        .get(`reports/get_report/${reportCode}`)
+        .end((err, response) => {
+          if (err) {
+            console.log(err);
+          }
+          //checking for various property to validate response object
+          response.should.have.status(200);
+          response.body.should.have.property("success").eql(true);
+          response.body.should.be.a("object");
+          response.body.data.should.have.property("report").be.a("object");
+          response.body.should.have.property("message");
+          response.body.message.should.be.eql("Report Received");
+          done();
+        });
+    });
+    //checking getting all report by passing invalid report code
+    it("Check for getting report by passing Report Code", (done) => {
+      let reportCode = "1234567890";
+      chai
+        .request("http://localhost:8000/")
+        .get(`reports/get_report/${reportCode}`)
+        .end((err, response) => {
+          if (err) {
+            console.log(err);
+          }
+          //checking for various property to validate response object
+          response.should.have.status(402);
+          response.body.should.have.property("success").eql(false);
+          response.body.should.be.a("object");
+          response.body.should.have.property("message");
+          response.body.message.should.be.eql("Report not found");
           done();
         });
     });
